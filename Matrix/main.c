b2031f5dec21b45d8e451ba57aca6c0dfb735b92
LI YANZHE <liyanzhe@duncan-lee-mba.local>
2016-11-30 17:54:08 +0800

    Initial Commit

---
+++ Matrix/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Matrix
+//
+//  Created by LI YANZHE on 30/11/2016.
+//  Copyright © 2016 Yanzhe Lee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
