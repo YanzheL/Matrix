481cef4a3e01d9426ff02469b21c821d6395f737
Yanzhe Lee <lee.yanzhe@yanzhe.org>
2017-02-04 14:16:23 +0800

    Bypass -o --mass-test in Windows

--- Matrix/main.c
+++ Matrix/main.c
@@ -28,8 +28,14 @@ int main(int argc, const char * argv[])
     if (Check_Option(argc, argv, "-c")!=0||Check_Option(argc, argv, "--config")!=0)configMode=1;
     if (Check_Option(argc, argv, "--mass-test")!=0)
     {
-        massFlag=1;
-        TEST_FLAG='1';
+#ifdef OSX
+		massFlag = 1;
+		TEST_FLAG = '1';
+#else
+		system("cls");
+		puts("\nMass Test mode is only available on UNIX platforms. Bypassing...");
+		return 0;
+#endif // OSX
     }
     if (invalidOptionFlag==0&&massFlag==0)
     {
@@ -50,8 +56,14 @@ int main(int argc, const char * argv[])
         }
         else
         {
-            outputMode=1;
-            puts("\nOutput success\n");
+#ifdef OSX
+			outputMode = 1;
+			puts("\nOutput success\n");
+#else
+			system("cls");
+			puts("\nOutput mode is only available on UNIX platforms. Bypassing...");
+			return 0;
+#endif // OSX
         }
     }
     if (Check_Option(argc, argv, "--test")!=0) TEST_FLAG='1';
